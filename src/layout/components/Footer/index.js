6e604152bc955877dec425aee81682c811e7f6a2
Dmitriy <dmitriycoupem@gmail.com>
2021-06-23 17:31:55 +0300

    Added main layout elements

--- src/layout/components/Footer/index.js
+++ src/layout/components/Footer/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import { FooterComponent } from './styledComponents';
 
 function Footer() {
-  return <FooterComponent>Footer</FooterComponent>;
+  return <FooterComponent>© 2021 My. Built My Theme.</FooterComponent>;
 }
+
+Footer.propTypes = {};
 
 export default Footer;
--- src/layout/components/Navigation/index.js
+++ src/layout/components/Navigation/index.js
@@ -1,26 +1,41 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+
 import { NavigationComponent, NavButton } from './styledComponets';
 
-function Navigation() {
-  const [converterActive, setConverterActive] = useState(false);
+function Navigation({ handleClickCourse, handleClickConverter }) {
+  const [converterActive, setConverterActive] = useState(true);
   const [courseActive, setCourseActive] = useState(false);
 
   return (
     <NavigationComponent>
       <NavButton
         primary={converterActive}
-        onClick={() => setConverterActive(!converterActive)}
+        onClick={() => {
+          handleClickConverter();
+          setConverterActive(true);
+          setCourseActive(false);
+        }}
       >
         Converter Button
       </NavButton>
       <NavButton
         primary={courseActive}
-        onClick={() => setCourseActive(!courseActive)}
+        onClick={() => {
+          handleClickCourse();
+          setConverterActive(false);
+          setCourseActive(true);
+        }}
       >
         Course Button
       </NavButton>
     </NavigationComponent>
   );
 }
+
+Navigation.propTypes = {
+  handleClickCourse: PropTypes.func,
+  handleClickConverter: PropTypes.func,
+};
 
 export default Navigation;
